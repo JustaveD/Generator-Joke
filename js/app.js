d67293d95638cdefc4305a96fb60fadd1348b4da
JustaveD <yeumytoan0101@gmail.com>
2021-04-18 17:46:25 +0700

    Manuplating DOM with js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 console.log('Got it!');
 
-// Get quote from API
+const jokeContainer = document.querySelector('#joke-container');
+const jokeText = document.querySelector('#joke');
+const jokeAuthor = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newJokeBtn = document.querySelector('#new-joke');
 
-async function getQuote(){
+// Get joke from API
+
+async function getJoke(){
     
     const apiUrl = 'https://api.chucknorris.io/jokes/random';
 
@@ -10,7 +16,17 @@ async function getQuote(){
         const response = await fetch(apiUrl);
         const data = await response.json();
         
-        console.log(data.value);
+        // If joke's length greater than 50 add long-joke class to it, else remove class
+        if(data.value.length > 50){
+            jokeText.classList.add('long-joke');
+        }
+        else{
+            jokeText.classList.remove('long-joke');
+        }
+        jokeText.innerText = data.value;
+
+        // Author alway isn't Duy, is it!
+        jokeAuthor.innerText = 'Isn\'t Duy ♫';
     }
     catch(error){
         getQuote();
@@ -18,6 +34,20 @@ async function getQuote(){
     }
 }
 
+// Tweet Joke
+function tweetJoke(){
+
+    const joke = jokeText.innerText;
+    const author = jokeAuthor.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${joke}-${author}`;
+
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newJokeBtn.addEventListener('click', getJoke);
+twitterBtn.addEventListener('click', tweetJoke);
+
 // On load
 
-getQuote();
+getJoke();
