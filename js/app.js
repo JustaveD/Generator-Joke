ed10dbe1e3c7fe2aec242121277bd84122110198
JustaveD <yeumytoan0101@gmail.com>
2021-04-18 17:05:02 +0700

    Because of cannot using quote api thats why I change generator quote to generator joke

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log('Got it!');
+
+// Get quote from API
+
+async function getQuote(){
+    
+    const apiUrl = 'https://api.chucknorris.io/jokes/random';
+
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        
+        console.log(data.value);
+    }
+    catch(error){
+        getQuote();
+        console.log('Whoops, no quote here ', error);
+    }
+}
+
+// On load
+
+getQuote();
